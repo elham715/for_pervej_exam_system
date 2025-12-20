c934df3ca9c90e238b2675f857174f1a755e4dca
parvej-shah <parvejshahlabib007@gmail.com>
2025-12-20 11:35:08 +0600

    feat: Update Firebase configuration to use environment variables and add hosting cache

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCF36FR8lWWhWIwbck6N9wrbkHdbPMakLg",
-  authDomain: "omni-exam-system.firebaseapp.com",
-  projectId: "omni-exam-system",
-  storageBucket: "omni-exam-system.firebasestorage.app",
-  messagingSenderId: "36840216568",
-  appId: "1:36840216568:web:daf8b4885ad64e9acba80d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
